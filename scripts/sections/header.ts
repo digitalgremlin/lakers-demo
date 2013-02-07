5302c57fbc1dfd4582a31ac02f1dd427e9046cb6
digitalgremlin <jslade@flammabletoys.com>
2013-02-07 09:06:17 -0800

    Changes I have made

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -6,4 +6,11 @@ $$('body') {
  $$("._header"){
  inject_top( read("header.html") )
  }
+ $$("#mw_header") {
+insert("div", class: "_icons_bar") {
+insert("div", class: "sprites-search")
+insert("div", class: "sprites-white-info")
+insert("div", class: "sprites-white-cart")
+}
+}
  }
