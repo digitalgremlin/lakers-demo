46457bda5a36769611d76e284d1a484faff4ae16
digitalgremlin <jslade@flammabletoys.com>
2013-02-08 23:39:56 -0800

    Changes I have made

--- scripts/pages/content.ts
+++ scripts/pages/content.ts
@@ -4,7 +4,7 @@ remove(".//br")
 table_dump(".//table")
 remove(".//div[@id='header']")
 remove(".//div[@id='breadcrumbholder']")
-
+remove(".//div[contains(concat(' ', @class, ' '), ' more-product-images ')]")
 $$("#bottommenu > div"){
 	add_class("_baller2")
 }
@@ -55,7 +55,61 @@ add_class("_item_price")
 move_to("//div[contains(concat(' ', @class, ' '), ' _product_head ')]", "bottom")
 }
 }
+$$("#product-image"){
+add_class("_product_image")
+}
 
+
+$$("._product_image > p"){
+$$("> a"){
+inner("TAP IMAGE TO ENLARGE")
+}
+}
+
+$$("._product_image > div"){
+remove()
+}
+
+$$(".product-body"){
+add_class("_product_body")
+}
+
+$$("._product_body"){
+$$("> p"){
+remove()
+}
+insert_top("p", "Details:")
+}
+
+
+$$("._product_body"){
+insert_after("div", class: "_share"){
+inject_top("Share:")
+insert("div", class: "_share_bar"){
+insert("div", class: "sprites-ls-share-facebook")
+insert("div", class: "sprites-ls-share-twitter")
+insert("div", class: "sprites-ls-share-google")
+insert("div", class: "sprites-ls-share-pinit")
+insert("div", class: "sprites-ls-share-linkedin")
+}
+}
+}
+$$(".sprites-ls-share-facebook"){
+add_class("_first")
+}
+
+$$(".meta"){
+add_class("_meta")
+}
+$$("._meta"){
+$$("ul"){
+insert_top("li", "Tags:")
+}
+}
+
+$$("#cartButtons .add-to-cart form > *:first-child"){
+add_class("_add_to_cart")
+}
 
 $$("#bottommenu"){
 	$$("._baller2") {
