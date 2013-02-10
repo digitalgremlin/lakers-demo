cf79606bf607cc5f17e4b7b7a1ff109417183b35
digitalgremlin <jslade@flammabletoys.com>
2013-02-10 03:03:31 -0800

    Changes I have made

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -7,19 +7,29 @@ $$('body') {
  inject_top( read("header.html") )
  }
  $$("#mw_header") {
+ attributes(data-ur-set: "tabs", data-ur-closeable: "true")
+
 insert("div", class: "_icons_bar") {
-insert("div", class: "sprites-white-search")
-insert("div", class: "sprites-white-info")
-insert("div", class: "sprites-white-cart")
+insert("div", class: "sprites-white-search", data-ur-tabs-component: "button", data-ur-tab-id: "search"){
+wrap("span")
+}
+insert("div", class: "sprites-white-info", data-ur-tabs-component: "button", data-ur-tab-id: "info"){
+wrap("span")
+}
+insert("div", class: "sprites-white-cart", data-ur-tabs-component: "button", data-ur-tab-id: "cart"){
+wrap("span")
+}
 }
 
 $$("#mw_logo"){
-insert("img", src: asset("images/logo.jpg"))
+insert("img", src: asset("images/logo.png"))
 }
 
- }
+}
+
 $$(".container-inline"){
-move_to("//header[@id='mw_header']", "bottom")
+attributes(data-ur-tabs-component: "content", data-ur-tab-id: "search")
+move_to("//div[@id='mw_bottom']", "bottom")
 add_class("_search")
 $$(".form-text"){
 attribute("value", "Search")
